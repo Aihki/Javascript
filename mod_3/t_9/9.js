c44dc49ce245f5122c0d0d3549c4e6b0d6fd8247
aihki <laine.nestori.julius@gmail.com>
2022-11-24 19:23:04 +0200

    korjattu

--- mod_3/t_9/9.js
+++ mod_3/t_9/9.js
@@ -5,22 +5,22 @@ document.querySelector("button").addEventListener("click", () => {
     
     if(calculation.includes('+')) {
         let numbers = calculation.split("+")
-        let results = numbers.reduce((acc,numbers) => acc + numbers);
+        let results = numbers.reduce((acc,numbers) => acc + parseInt(numbers),0);
         document.querySelector("#result").textContent = results 
     }        
     else if(calculation.includes('-')) {
         let numbers = calculation.split("-")
-        let results = numbers.reduce((acc,numbers) => acc - numbers);
+        let results = numbers.reduce((acc,numbers) => acc - parseInt(numbers));
         document.querySelector("#result").textContent = results 
     }
     else if(calculation.includes('*')) {
         let numbers = calculation.split("*")
-        let results = numbers.reduce((acc,numbers) => acc * numbers);
+        let results = numbers.reduce((acc,numbers) => acc * parseInt(numbers));
         document.querySelector("#result").textContent = results 
     }
     else if(calculation.includes('/')) {
         let numbers = calculation.split("/")
-        let results = numbers.reduce((acc,numbers) => acc / numbers);
+        let results = numbers.reduce((acc,numbers) => acc / parseInt(numbers));
         document.querySelector("#result").textContent = results 
     }
 
