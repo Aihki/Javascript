be9e70feb859ec24af46866623a4ddc95bb19be9
Aihki <laine.nestori.julius@gmail.com>
2022-11-24 19:50:41 +0200

    muutoksia lisaa

--- mod_3/t7/7.js
+++ mod_3/t7/7.js
@@ -1,17 +1,15 @@
-document.querySelector("#target").addEventListener("mouseover", () => {
-  document.querySelector("#target").src = "./img/picB.jpg"
-  })
-document.querySelector("#target").addEventListener("mouseleave", () => {
-  document.querySelector("#target").src = "./img/picA.jpg";
-  })
-  
-
-
-
 /*document.querySelector("#trigger").addEventListener("mouseover", () => {
   document.querySelector("#target").src = "./img/picB.jpg"
   });
 document.querySelector("#trigger").addEventListener("mouseleave", () => {
   document.querySelector("#target").src = "./img/picA.jpg";
   });*/
+  
+
+document.querySelector("#target").addEventListener("mouseover", () => {
+  document.querySelector("#target").src = "./img/picB.jpg"
+  })
+document.querySelector("#target").addEventListener("mouseleave", () => {
+  document.querySelector("#target").src = "./img/picA.jpg";
+  })
   
